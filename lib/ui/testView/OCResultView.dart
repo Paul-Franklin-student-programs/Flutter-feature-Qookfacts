90a0a3a68fa2a4b550a9bbe948c4080e2fb32bf1
santosh <santosh@santoshs-mini.lan>
2023-10-18 11:17:20 -0700

    add + button

--- lib/ui/testView/OCResultView.dart
+++ lib/ui/testView/OCResultView.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:qookit/services/theme/theme_service.dart';
 
+
+
 class OCRResultView extends StatefulWidget {
   final String ocrResults;
 
@@ -12,6 +14,46 @@ class OCRResultView extends StatefulWidget {
 }
 
 class _OCRResultViewState extends State<OCRResultView> {
+  // Initialize a list to hold the data.
+  List<String> contentData = [];
+
+  // Initialize a flag to track if you're currently loading data.
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // Add the initial content from widget.ocrResults to contentData.
+    contentData.add(widget.ocrResults);
+  }
+
+  // Method to make an API call and append new data.
+  Future<void> loadMoreData() async {
+    if (isLoading) {
+      return;
+    }
+
+    setState(() {
+      isLoading = true;
+    });
+
+    // Make your API call here, e.g., using http package.
+    final newData = await makeApiCall();
+
+    // Append the new data to the existing content.
+    setState(() {
+      contentData.addAll(newData.split('\n'));
+      isLoading = false; // Set isLoading to false when data is loaded.
+    });
+  }
+
+  // Method to make an API call.
+  Future<String> makeApiCall() async {
+    // Replace this with your actual API call logic.
+    await Future.delayed(Duration(seconds: 2));
+    return 'New data from API'; // Replace with your API response.
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,13 +65,23 @@ class _OCRResultViewState extends State<OCRResultView> {
           centerTitle: true,
           backgroundColor: qookitLight.primaryColor, // Set the app bar color to qookitLight's primary color
         ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Text(
-              '${widget.ocrResults}', // Updated text
-              style: TextStyle(fontSize: 18),
-            ),
-          ),
+        body: ListView.builder(
+          itemCount: contentData.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(
+                contentData[index],
+                style: TextStyle(fontSize: 18),
+              ),
+            );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Call the method to load more data when the button is pressed.
+            loadMoreData();
+          },
+          child: Icon(Icons.add),
         ),
       ),
     );
